efb31a512a9216e61a2d23cf777e8a2e8bd5204f
Ubuntu <hackamvigo@Antigravity.Sword-15>
2026-04-05 14:43:42 +0300

    chore: remove .next from tracking and update .gitignore

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/page.tsx -> idb-keyval\":{\"id\":\"app/page.tsx -> idb-keyval\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_idb-keyval_dist_index_js.js\"]}}"
